6970d0dd6ba840c202a02cbc1b48aa2ce5cedcce
Sirisha Pratha <sirisha.pratha@bnymellon.com>
2021-05-16 14:40:15 -0400

    Initial commit for user interface plugin demo

---
+++ user-interface-demo/src/main/java/actions/SearchGithubAction.java
@@ -0,0 +1,36 @@
+package actions;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.DumbAwareAction;
+import org.jetbrains.annotations.NotNull;
+
+public class SearchGithubAction extends DumbAwareAction
+{
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e)
+    {
+        Editor ediTorRequiredData = e.getRequiredData(CommonDataKeys.EDITOR);
+        CaretModel caretModel = ediTorRequiredData.getCaretModel();
+        String selectedText = caretModel.getCurrentCaret().getSelectedText();
+
+        String gitHubQuery = "https://github.com/search?q=%s&type=code";
+        String searchBy = String.format(gitHubQuery, selectedText);
+        BrowserUtil.browse(searchBy);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e)
+    {
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        CaretModel caretModel = editor.getCaretModel();
+        String selectedText = caretModel.getCurrentCaret().getSelectedText();
+        if (selectedText.isEmpty())
+        {
+            e.getPresentation().setVisible(false);
+        }
+    }
+}
